b8aff4f912354e5eb2203f904c951680543071c0
kingaer04 <danielanifowoshe04@gmail.com>
2022-09-12 16:16:04 -0700

    jh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: using the main function
  * this program prints "prog
- * Retun: 0
+ * Return: 0
  */
 int main(void)
 {
@@ -15,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("size of a char:%ld byte(s)\n", sizeof(c));
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %ld byte(s)\n", sizeof(i));
-	printf("size of long int: %ld byte(s)\n", sizeof(li));
-	printf("size of long long int: %ld byte(s)\n", sizeof(lli));
-	printf("size of float: %ld byte(s)\n", sizeof(f));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
